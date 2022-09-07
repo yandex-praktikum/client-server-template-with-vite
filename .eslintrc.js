dca5882ee4894b360f05ed4867a06bf6d02af5d9
Anton Majmistov <antonmajmistov@yandex.ru>
2022-09-07 23:20:51 +0300

    #1 - updating repo building

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,131 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
-    node: true,
+    node: true
   },
   extends: [
     'eslint:recommended',
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     'plugin:@typescript-eslint/recommended',
-    'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:sonarjs/recommended',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 11
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'sonarjs'],
   rules: {
     '@typescript-eslint/ban-ts-comment': 1,
-  },
+    '@typescript-eslint/no-unsafe-assignment': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    'comma-dangle': [
+      'error',
+      {
+        'arrays': 'always-multiline',
+        'objects': 'always-multiline',
+        'imports': 'always-multiline',
+        'exports': 'always-multiline',
+        'functions': 'only-multiline'
+      }
+    ],
+    'eol-last': [
+      'error',
+      'always'
+    ],
+    'import/default': 2,
+    'import/export': 2,
+    'import/named': 2,
+    'import/namespace': [
+      2,
+      {
+        'allowComputed': false
+      }
+    ],
+    'import/no-restricted-paths': 2,
+    'import/order': [
+      'error',
+      {
+        'alphabetize': {
+          'order': 'asc',
+          'caseInsensitive': true
+        },
+        'groups': [
+          'external',
+          'internal',
+          'index',
+          'sibling',
+          'parent',
+          'builtin',
+          'object'
+        ],
+        'newlines-between': 'always'
+      }
+    ],
+    'max-len': [
+      'error',
+      {
+        'code': 120,
+        'ignoreStrings': true,
+        'ignoreTemplateLiterals': true,
+        'ignoreRegExpLiterals': true
+      }
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        'max': 1
+      }
+    ],
+    'no-trailing-spaces': [
+      'error'
+    ],
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'padding-line-between-statements': [
+      'error',
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': 'return'
+      },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': 'block'
+      },
+      {
+        'blankLine': 'always',
+        'prev': 'block',
+        'next': '*'
+      },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': 'block-like'
+      },
+      {
+        'blankLine': 'always',
+        'prev': 'block-like',
+        'next': '*'
+      }
+    ],
+    'quotes': [
+      'error',
+      'single',
+      {
+        'avoidEscape': true
+      }
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    '@typescript-eslint/no-misused-promises': 0
+  }
 }
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,15 @@
-import { useEffect } from 'react'
-import './App.css'
+import { useEffect } from 'react';
 
-function App() {
+export function App(): JSX.Element {
   useEffect(() => {
     const fetchServerData = async () => {
-      const response = await fetch('http://localhost:3001')
-      const data = await response.json()
-      console.log(data)
-    }
+      const response = await fetch('http://localhost:3001');
+      const data = await response.json();
+      console.log(data);
+    };
 
-    fetchServerData()
-  }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+    fetchServerData();
+  }, []);
+
+  return <div className='App'>Вот тут будет жить ваше приложение :)</div>;
 }
-
-export default App
