0872e063e24bc26b1477b9bdc3cbc1641c629761
tmko16 <simontis1991@gmail.com>
2022-10-02 11:49:48 +0300

    feat: Добавлен rtk, сконфигурирован стор и базовый слайс(ветка стора).

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,13 +2,17 @@ import 'normalize.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import { App } from './App';
+import { store } from './store/store';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
---
+++ packages/client/src/store/commonSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface CommonState {
+  currentUser: string,
+  theme: string,
+  language: string,
+}
+
+const initialState: CommonState = {
+  currentUser: 'Yolo man',
+  theme: 'default',
+  language: 'RU',
+};
+
+export const counterSlice = createSlice({
+  name: 'common',
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.currentUser = action.payload;
+    },
+    setTheme: (state, action) => {
+      state.theme = action.payload;
+    },
+    setLanguage: (state, action) => {
+      state.language = action.payload;
+    },
+  },
+});
+
+export const { setUser, setTheme, setLanguage } = counterSlice.actions;
+
+export default counterSlice.reducer;
