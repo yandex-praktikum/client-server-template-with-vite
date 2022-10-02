70ac4ecd3fb5ca883b902a780d1024221479ed75
tmko16 <simontis1991@gmail.com>
2022-10-02 12:48:12 +0300

    feat: Добавил RTK QUERY в store, создал базовый апи слайс.

--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import  commonReducer from './commonSlice';
+
+import { api } from '../services/api';
 export const store = configureStore({
   reducer: {
     common: commonReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
