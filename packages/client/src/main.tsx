debf15b1c80a0631c60c6d35de21147fd864c714
Elena Shashina <elvehnn@yandex.ru>
2022-12-05 16:25:17 +0300

    refactor: move ConfigProvider to the main.tsx

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,16 +1,20 @@
+import { ConfigProvider } from "antd";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { appTheme } from "./constants/appTheme";
 import "./index.css";
 import { ErrorBoundary } from "./pages/errorPages/ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
-            <ErrorBoundary>
-                <App />
-            </ErrorBoundary>
+            <ConfigProvider theme={appTheme}>
+                <ErrorBoundary>
+                    <App />
+                </ErrorBoundary>
+            </ConfigProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
