ae79ce6f1466b98a1b54b1e016c83895f7ddd50f
Elena Shashina <elvehnn@yandex.ru>
2022-12-07 14:45:41 +0300

    feat: connect route paths to menu items

--- packages/client/src/components/navigation/Navigation.tsx
+++ packages/client/src/components/navigation/Navigation.tsx
@@ -1,47 +1,68 @@
 import {
-  LogoutOutlined,
-  TrophyOutlined,
-  UserOutlined,
+    LogoutOutlined,
+    TeamOutlined,
+    TrophyOutlined,
+    UserOutlined,
 } from "@ant-design/icons";
 import { Menu } from "antd";
 import type { MenuProps } from "antd/es/menu";
+import { NavLink } from "react-router-dom";
 import "./Navigation.scss";
 
 type MenuItem = Required<MenuProps>["items"][number];
 
 function getItem(
-  label: React.ReactNode,
-  key?: React.Key | null,
-  icon?: React.ReactNode,
-  className = "navigation__item"
+    label: React.ReactNode,
+    key?: React.Key | null,
+    icon?: React.ReactNode,
+    className = "navigation__item"
 ): MenuItem {
-  return {
-    key,
-    icon,
-    label,
-    className,
-  } as MenuItem;
+    return {
+        key,
+        icon,
+        label,
+        className,
+    } as MenuItem;
 }
 
 const items: MenuItem[] = [
-  getItem("View profile", "1", <UserOutlined />),
-  getItem("View leaderboard", "2", <TrophyOutlined />),
-  getItem("View forum", "4", <TrophyOutlined />),
-  getItem("Log out", "4", <LogoutOutlined />),
+    getItem(
+        "View profile",
+        "1",
+        <NavLink to={"/profile"}>
+            <UserOutlined />
+        </NavLink>
+    ),
+    getItem(
+        "View leaderboard",
+        "2",
+        <NavLink to={"/ladder"}>
+            <TrophyOutlined />
+        </NavLink>
+    ),
+    getItem(
+        "View forum",
+        "3",
+        <NavLink to={"/forum"}>
+            <TeamOutlined />
+        </NavLink>
+    ),
+    getItem(
+        "Log out",
+        "4",
+        <NavLink to={"/sign-in"}>
+            <LogoutOutlined />
+        </NavLink>
+    ),
 ];
 
 export const NavigationMenu = () => {
-  const handleClick = (event: any) => {
-    console.log("click ", event);
-  };
-
-  return (
-    <Menu
-      className="navigation"
-      onClick={handleClick}
-      items={items}
-      mode="vertical"
-      style={{ borderInlineEnd: "none" }}
-    />
-  );
+    return (
+        <Menu
+            className="navigation"
+            items={items}
+            mode="vertical"
+            style={{ borderInlineEnd: "none" }}
+        />
+    );
 };
