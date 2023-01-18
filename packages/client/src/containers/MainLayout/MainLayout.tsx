580896428ccdf7d878eba686d7713e15ceb01c42
Elena Shashina <elvehnn@yandex.ru>
2023-01-18 21:43:48 +0300

    refactor: change styles and layout

--- packages/client/src/containers/MainLayout/MainLayout.tsx
+++ packages/client/src/containers/MainLayout/MainLayout.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent, ReactElement } from "react";
 import { Layout, Row, Col, Button } from "antd";
 import "./MainLayout.sass";
 import { NavigationMenu } from "@/components/navigation/Navigation";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./MainPage.scss";
 import { useAppSelector } from "@/store/hooks";
 import { userSelectors } from "@/store/slices/user/userSlice";
@@ -15,7 +15,6 @@ type MainLayoutProps = {
 };
 
 const MainLayout: FunctionComponent<MainLayoutProps> = ({ children }) => {
-    const { pathname } = useLocation();
     const { user } = useAppSelector(userSelectors.all);
 
     return (
@@ -38,7 +37,9 @@ const MainLayout: FunctionComponent<MainLayoutProps> = ({ children }) => {
                             alignItems: "center",
                             justifyContent: "flex-start",
                         }}>
-                        <Title>Привет, {user ? user.login : "Юзер"}!</Title>
+                        <Title level={2}>
+                            Привет, {user ? user.login : "Юзер"}!
+                        </Title>
 
                         {user ? (
                             <Title level={3}>Твой лучший результат: 777</Title>
@@ -46,20 +47,17 @@ const MainLayout: FunctionComponent<MainLayoutProps> = ({ children }) => {
 
                         <NavigationMenu />
 
-                        {pathname === "/" ? null : (
-                            <NavLink to={"/"}>
-                                <Button
-                                    size="large"
-                                    htmlType="button"
-                                    type="primary"
-                                    className="layout_content-start">
-                                    СТАРТ
-                                </Button>
-                            </NavLink>
-                        )}
+                        <NavLink to={"/game"}>
+                            <Button
+                                htmlType="button"
+                                type="primary"
+                                className="layout_content-start">
+                                СТАРТ
+                            </Button>
+                        </NavLink>
                     </Col>
 
-                    <Col span={17} className="layout_content-child">
+                    <Col span={15} className="layout_content-child">
                         {children}
                     </Col>
                 </Row>
