ba238cd9832c5c9503d76f9293d24b2839bbef5b
Ivan <yanickiy.ivan@mail.ru>
2023-09-18 12:57:43 +0400

    Добавлен роутинг, в качестве элементво установлены заглушки App

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<App />} />
+        <Route path="/registration" element={<App />} />
+        <Route path="/game" element={<App />} />
+        <Route path="/profile" element={<App />} />
+        <Route path="/leaderboard" element={<App />} />
+        <Route path="/forum" element={<App />} />
+        <Route path="/topic" element={<App />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
