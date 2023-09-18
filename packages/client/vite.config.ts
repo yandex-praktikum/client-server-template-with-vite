867384406e8e1866c3ec159dd0f62e5d875735ec
Andrey Kochanov <kochanov@lanbilling.ru>
2023-09-18 20:04:50 +0300

    PRA-20__Make_signUp_page
    - Сделал архитектуру папок
    - Настроил работу со стилями
    - Добавил тему
    - Сверстал страницу регистрации
    - Настроил алиасы
    - Добавил шрифты

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -10,6 +11,15 @@ export default defineConfig({
   },
   define: {
     __SERVER_PORT__: process.env.SERVER_PORT,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@fonts': path.resolve(__dirname, './src/fonts'),
+    },
   },
   plugins: [react()],
 })
