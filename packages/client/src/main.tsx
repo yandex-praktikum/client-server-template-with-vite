742550ed468354922b7956d6622f461c00253f94
Litvinova Galina <litvinovaga@inbox.ru>
2023-09-23 14:40:51 +0300

    Страница с логином

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import '@styles/styles.less'
+import Login from './pages/login/login'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/login" element={<App />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<App />} />
         <Route path="/game" element={<App />} />
         <Route path="/profile" element={<App />} />
---
+++ packages/client/src/pages/login/login.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import classes from './styles.module.less'
+import { Button, Form, Input } from 'antd'
+import TetrisImg from './components/tetrisImg/Tetris'
+
+const Login: React.FC = () => {
+  return (
+    <div className={classes.login}>
+      <TetrisImg />
+      <div className={classes.login__form}>
+        <span className={classes.title}>Welcome to Tetris</span>
+        <Form>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            colon={false}
+            label={<span>Login</span>}
+            name="login">
+            <Input placeholder="Login" />
+          </Form.Item>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            colon={false}
+            label={<span>Password</span>}
+            name="password">
+            <Input placeholder="Password" type="password" />
+          </Form.Item>
+          <button
+            className={classes.login__btn}
+            style={{
+              width: '100%',
+            }}>
+            Login
+          </button>
+          <Button
+            type="link"
+            style={{
+              width: '100%',
+            }}>
+            Don’t have an account?
+          </Button>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
