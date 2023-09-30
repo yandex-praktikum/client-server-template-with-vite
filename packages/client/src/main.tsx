29c8b0d136af2ecf131ca047d95a2d2e9c8cba26
IvanYank <61747305+IvanYank@users.noreply.github.com>
2023-09-30 23:36:39 +0400

    Update main.tsx

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import '@styles/styles.less'
-import Canvas from './pages/canvas/canvas'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
