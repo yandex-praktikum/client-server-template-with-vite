1701764763e8cad05d40cb8dee55e6b7729c3c60
EvgeniyGrokholskiy <grokholskiy@gmail.com>
2023-10-15 11:12:06 +0500

    feature_(formInput): add formInput_15.10.23.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,7 @@
 import React, { FC, useEffect } from 'react'
+import { Form, Formik } from 'formik'
+import { FormInput } from './components/formInput'
+import './App.scss'
 
 const App: FC = () => {
   useEffect(() => {
@@ -11,7 +14,43 @@ const App: FC = () => {
 
     fetchServerData()
   }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+  return (
+    <div className="App">
+      <div>
+        <h1>Вход</h1>
+        <Formik
+          initialValues={{
+            login: '',
+            password: '',
+          }}
+          onSubmit={async values => {
+            await new Promise(r => setTimeout(r, 500))
+            alert(JSON.stringify(values, null, 2))
+          }}>
+          <Form>
+            <br />
+            <FormInput
+              type="text"
+              id="login"
+              name="login"
+              labelText="Логин"
+              placeholder="Введите логин"
+            />
+            <br />
+            <FormInput
+              id="password"
+              name="password"
+              type="password"
+              labelText="Пароль"
+              placeholder="Введите пароль"
+            />
+            <br />
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  )
 }
 
 export default App
