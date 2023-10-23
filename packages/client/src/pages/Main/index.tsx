2bcaf2070b8b9b9aca54ae274bbd821e2472a949
Sherstneva Tatyana <nikolina.tatyana@yandex.ru>
2023-10-23 18:44:03 +0500

    добавила стр MainPage

--- packages/client/src/pages/Main/index.tsx
+++ packages/client/src/pages/Main/index.tsx
@@ -1,16 +1,46 @@
 import React from 'react'
-import { ROUTES_NAMES } from '../../const/routeNames'
+import s from './index.module.scss'
 import { Link } from 'react-router-dom'
+import { ROUTES_NAMES } from '../../const/routeNames'
+
 type TMainPage = {
   logoutCallback: () => void
 }
 
 const MainPage = ({ logoutCallback }: TMainPage) => {
   return (
-    <div>
-      MainPage
-      <button onClick={logoutCallback}>Logout</button>
-      <Link to={ROUTES_NAMES.LEADER_BOARD}>Доска лидеров</Link>
+    <div className={s.wrapper}>
+      <div className={s.container}>
+        <header className={s.header}>
+          <button onClick={logoutCallback} className={s.btn}>
+            Выйти
+          </button>
+          <div className={s.avatar}>
+            <img src="/src/assets/avatar.svg" alt="avatar" />
+          </div>
+        </header>
+
+        <main className={s.main}>
+          <div className={s.btn_wrapper}>
+            <Link to={ROUTES_NAMES.GAME} className={`${s.btn} ${s.btn_play}`}>
+              Играть
+            </Link>
+            <Link to={ROUTES_NAMES.FORUM} className={`${s.btn} ${s.btn_forum}`}>
+              Форум
+            </Link>
+            <Link
+              to={ROUTES_NAMES.SETTINGS}
+              className={`${s.btn} ${s.btn_profile}`}>
+              Профиль
+            </Link>
+            <Link
+              to={ROUTES_NAMES.LEADER_BOARD}
+              className={`${s.btn} ${s.btn_leaders}`}>
+              Доска лидеров
+            </Link>
+          </div>
+        </main>
+      </div>
     </div>
   )
 }
