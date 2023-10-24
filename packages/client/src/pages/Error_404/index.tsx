a3857fb28714261b9c0273684567b85047e1efc2
EvgeniyGrokholskiy <grokholskiy@gmail.com>
2023-10-25 03:06:24 +0500

    fix_(APP): fix errors_25.10.23.

--- packages/client/src/pages/Error_404/index.tsx
+++ packages/client/src/pages/Error_404/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './index.module.scss'
-import { ErrorPageLink } from '../../components/errorPageLink'
+import { ErrorPageLink } from '../../components/ErrorPageLink'
 
 const Error404 = () => {
   return (
