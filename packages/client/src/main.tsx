3e0e1d71f6f733e46872195b3d661d0ed1fa7008
Egor Ryazanov <egor.ryazanov@saritasa.com>
2023-11-05 18:38:11 +0500

    Добавил service worker

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -14,3 +14,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </Provider>
   </React.StrictMode>
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js', { scope: '/', type: 'module' })
+  })
+}
