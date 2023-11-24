8ee6f33c00825ed343e61b310adfa48b25e1719f
EvgeniyGrokholskiy <grokholskiy@gmail.com>
2023-11-25 00:00:47 +0500

    fix_(LeaderBoard): remove input fpom page_24.11.23.

--- packages/client/src/pages/LeaderBoard/index.tsx
+++ packages/client/src/pages/LeaderBoard/index.tsx
@@ -1,32 +1,19 @@
 import React, { useEffect } from 'react'
 
-import { setNewScore } from '../../store/leaderBoard/slice'
 import { useAppDispatch, useAppSelector } from '../../hook/hook'
 import { LeaderBoardItem } from '../../components/LeaderBoardItem'
 import { getLeaderBoardPageData } from '../../store/leaderBoard/selectors'
-import {
-  getLeaderBoardThunk,
-  addNewUserToLeadBoard,
-} from '../../store/leaderBoard/dispatchers'
+import { getLeaderBoardThunk } from '../../store/leaderBoard/dispatchers'
 
 import s from './index.module.scss'
 
 const LeaderBordPage = () => {
   const dispatch = useAppDispatch()
-  const { data, newScore } = useAppSelector(getLeaderBoardPageData)
+  const { data } = useAppSelector(getLeaderBoardPageData)
 
   useEffect(() => {
     dispatch(getLeaderBoardThunk())
   }, [dispatch])
-
-  const addLeadHandler = () => {
-    dispatch(addNewUserToLeadBoard(+newScore))
-  }
-
-  const setNewScoreHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.currentTarget
-    dispatch(setNewScore(value))
-  }
 
   const leaderBoardItems = data.map(({ name, score, avatar }, index) => (
     <LeaderBoardItem
@@ -42,10 +29,6 @@ const LeaderBordPage = () => {
     <>
       <h1 className={s.title}>Доска лидеров</h1>
       <div className={s.table}>{leaderBoardItems}</div>
-      <div className={s.wrapper}>
-        <input value={newScore} type={'number'} onChange={setNewScoreHandler} />
-        <button onClick={addLeadHandler}>AddLead</button>
-      </div>
     </>
   )
 }
